4ee050faec66377d9c129fd4a2e8a19f537bd3a8
Thomas Steen Rasmussen <thomas.rasmussen@bornfiber.dk>
2019-02-04 20:10:29 +0100

    add another template filter

--- acsadmin/templatetags/custom_template_filters.py
+++ acsadmin/templatetags/custom_template_filters.py
@@ -15,3 +15,11 @@ def pretty_timedelta(value):
         # return a pretty printed version without microseconds
         return str(timedelta(seconds=int(value.total_seconds())))
 
+@register.filter
+def truefalseicon(value):
+    '''Returns a icon with a green checkmark or a red X depending on true/false input, requires font-awesome to be any good'''
+    if value:
+        return '<i class="fa fa-check" style="color:#00947d"></i>'
+    else:
+        return '<i class="fa fa-times" style="color:#494849"></i>'
+
